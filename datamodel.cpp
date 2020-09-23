36c0c966ee67a71bd353e54d5fa85b4c31dafd93
jurplel <jeep70cp@gmail.com>
2020-09-23 17:40:36 +0000

    Cheating on sorting and still using a struct

--- datamodel.cpp
+++ datamodel.cpp
@@ -7,72 +7,32 @@ DataModel::DataModel()
 
 }
 
+static void print_all_devices(std::vector<UsbDevice> &dev_list)
+{
+    for (const auto &device : std::as_const(dev_list))
+    {
+        std::cout << device << std::endl;
+        std::cout << "------" << std::endl;
+    }
+}
+
 void DataModel::sort_usb_devices(std::vector<UsbDevice> &dev_list)
 {
-    std::sort(std::begin(dev_list), std::end(dev_list));
+    std::sort(std::begin(dev_list), std::end(dev_list), [](UsbDevice dev0, UsbDevice dev1){
+        return dev0.size_in_megabytes < dev1.size_in_megabytes;
+    });
 }
 
 void DataModel::run()
 {
-    UsbDevice bt_adapter;
-    bt_adapter.name = "BT-400";
-    bt_adapter.manufacturer = "Premiertek";
-    bt_adapter.chipset = "Broadcom BCM20702";
-    bt_adapter.connector_type = 'A';
-    bt_adapter.usb_version = 2.0f;
-
-    UsbDevice::Bluetooth btdt;
-    btdt.bt_version = 4.0;
-    btdt.is_le = true;
-
-    bt_adapter.device_type = btdt;
-    usb_devices.push_back(bt_adapter);
-
-    UsbDevice sandisk_usb_drive = {
+    usb_devices.push_back({
         "Cruzer",
         "Sandisk",
         "",
         'A',
         2.0f,
-        UsbDevice::Storage {
-            58704,
-            true
-        }
-    };
-    usb_devices.push_back(sandisk_usb_drive);
-
-    usb_devices.push_back({
-        "F9L1001v1",
-        "Belkin",
-        "Realtek RTL8188SU",
-        'A',
-        2.0f,
-        UsbDevice::Wifi {
-            {'n', 'g', 'b'},
-            0
-        }
-    });
-
-    usb_devices.push_back({
-        "G502 HERO SE",
-        "Logitech",
-        "Logitech Hero16k",
-        'A',
-        2.0f,
-        UsbDevice::Peripheral {
-            1000
-        }
-    });
-
-    usb_devices.push_back({
-        "ANHA-011PW",
-        "Essential Products",
-        "",
-        'C',
-        0.0,
-        UsbDevice::Audio {
-            true
-        }
+        58704,
+        true
     });
 
     usb_devices.push_back({
@@ -81,15 +41,25 @@ void DataModel::run()
         "Toshiba MLC",
         'A',
         3.0f,
-        UsbDevice::Storage {
-            15112,
-            true
-        }
+        15112,
+        true
     });
 
-    for (const auto &device : std::as_const(usb_devices))
-    {
-        std::cout << device << std::endl;
-        std::cout << "------" << std::endl;
-    }
+    usb_devices.push_back({
+        "",
+        "Dongcoh",
+        "Western Digital",
+        'A',
+        3.0f,
+        610352,
+        true
+    });
+
+    print_all_devices(usb_devices);
+
+    std::cout << "-----------------------------------------------\n";
+
+    sort_usb_devices(usb_devices);
+
+    print_all_devices(usb_devices);
 }
--- datamodel.h
+++ datamodel.h
@@ -17,89 +17,20 @@ public:
     char connector_type; // a b or c
     float usb_version; // 2.0, 3.0, 3.1, 3.12, etc
 
-    struct Storage 
-    { 
-        int size_in_megabytes;
-        bool is_flash_based;
-    };
-    struct Wifi 
-    { 
-        std::vector<char> protocols;
-        int antenna_count;
-    };
-    struct Bluetooth 
-    {
-        float bt_version;
-        bool is_le;
-    };
-    struct Audio 
-    {
-        bool is_trrs;
-    };
-    struct Peripheral 
-    {
-        int polling_rate;
-    };
-    struct PowerDelivery 
-    {
-        bool is_outgoing; // if false, power is incoming
-        int wattage;
-    };
-    struct Other {};
-
-    std::variant<Storage,
-        Wifi,
-        Bluetooth,
-        Audio,
-        Peripheral,
-        PowerDelivery,
-        Other> device_type;
+    int size_in_megabytes;
+    bool is_flash_based;
 };
 
 inline std::ostream& operator<<(std::ostream &o, const UsbDevice dev)
 {
-    std::string type_string;
-    if (auto pval = std::get_if<UsbDevice::Storage>(&dev.device_type)) {
-        std::string flashString;
-        if (pval->is_flash_based)
-            flashString = "Flash Memory";
-        type_string = std::to_string(pval->size_in_megabytes) + " MB " + flashString;
-    } else if (auto pval = std::get_if<UsbDevice::Wifi>(&dev.device_type)) {
-        type_string = std::to_string(pval->antenna_count) + " Antenna(s) ";
-        for (const auto &protocol : std::as_const(pval->protocols)) {
-            type_string += protocol;
-            type_string += '/';
-        }
-    } else if (auto pval = std::get_if<UsbDevice::Bluetooth>(&dev.device_type)) {
-        type_string = "Bluetooth " + std::to_string(pval->bt_version);
-        if (pval->is_le)
-            type_string += " w/ LE";
-    } else if (auto pval = std::get_if<UsbDevice::Audio>(&dev.device_type)) {
-        type_string = "TRRS: ";
-        if (pval->is_trrs) {
-            type_string += "true";
-        } else {
-            type_string += "false";
-        }
-    } else if (auto pval = std::get_if<UsbDevice::Peripheral>(&dev.device_type)) {
-        type_string = std::to_string(pval->polling_rate) + " Hz";
-    } else if (auto pval = std::get_if<UsbDevice::PowerDelivery>(&dev.device_type)) {
-        type_string = "Mode: ";
-        if (pval->is_outgoing) {
-            type_string += "Sending ";
-        } else {
-            type_string += "Receiving ";
-        }
-        type_string += std::to_string(pval->wattage);
-        type_string += " W";
-    } else if (auto pval = std::get_if<UsbDevice::Other>(&dev.device_type)) {
-        type_string = "Device details unknown";
-    }
+    std::string flashString;
+    if (dev.is_flash_based)
+        flashString = "Flash Memory";
 
     return o << "Name: " << dev.manufacturer << ' ' << dev.name << std::endl
             << "Connector: " << dev.connector_type << ' ' << std::to_string(dev.usb_version) << std::endl
             << "Chipset: " << dev.chipset << std::endl
-            << type_string;
+            << std::to_string(dev.size_in_megabytes) << " MB " << flashString;
 }
 
 class DataModel : Project 
