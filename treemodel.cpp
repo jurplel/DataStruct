590028aad8c3fc9145d9c818355f399fb20e6d56
Benjamin O <jeep70cp@gmail.com>
2020-10-12 16:16:57 -0400

    Implemented a lot but not working cause pointers

--- treemodel.cpp
+++ treemodel.cpp
@@ -1,12 +1,66 @@
 #include "treemodel.h"
 
+#include <iostream>
+#include <random>
+
+//To-do: destructors for pointers
+
 TreeModel::TreeModel()
 {
    root_node = nullptr; 
 }
 
+static std::pair<Node*, Node*> find_place_for_value(const unsigned __int128 &val, Node *candidate_node)
+{
+    Node *place;
+    if (val > candidate_node->get_val())
+        place = candidate_node->get_child1();
+    else
+        place = candidate_node->get_child0();
+
+    if (!place)
+        return {place, candidate_node};
+
+    return find_place_for_value(val, place);
+}
+
+void TreeModel::populate_nodes(int root_index)
+{
+    root_node = new Node(PLANET_INFO[root_index].first, PLANET_INFO[root_index].second, nullptr);
+
+    for (int i = 0; i < PLANET_INFO.size(); i++)
+    {
+        if (i == root_index)
+            continue;
+
+        auto [new_pos, new_parent] = find_place_for_value(PLANET_INFO[i].second, root_node);
+        new_pos = new Node(PLANET_INFO[i].first, PLANET_INFO[i].second, new_parent);
+    }
+}
+
+static void print_recursive_child_nodes(Node *node, int indentation = 0)
+{
+    for (int i = 0; i < indentation; i++)
+        std::cout << '\t';
+    
+    std::cout << node->get_key() << ' ' << std::endl;
+    
+    if (node->get_child0())
+        print_recursive_child_nodes(node->get_child0(), indentation + 1);
+    
+    if (node->get_child1())
+        print_recursive_child_nodes(node->get_child1(), indentation + 1);
+}
+
 void TreeModel::run()
 {
-    root_node = new Node("io", 89.32e21, nullptr);
+    std::random_device random_device;
+    std::mt19937 generator(random_device());
+    std::uniform_int_distribution<int> distribution(0, PLANET_INFO.size());
 
+    int random_index = distribution(generator);
+    std::cout << "root index " << random_index << std::endl;
+
+    populate_nodes(random_index);
+    print_recursive_child_nodes(root_node);
 }
--- treemodel.h
+++ treemodel.h
@@ -2,12 +2,19 @@
 #define TREEMODEL_H
 
 #include <string>
+#include <vector>
 
 #include "project.h"
 
 class Node {
 public:
-    explicit Node(const std::string &_key, unsigned __int128 _val, Node *_parent = nullptr) : key(_key), val(_val), parent(_parent) {};
+    explicit Node(const std::string &_key, unsigned __int128 _val, Node *_parent = nullptr) : key(_key), val(_val), parent(_parent), child0(nullptr), child1(nullptr) {};
+
+    std::string get_key() const { return key; };
+    unsigned __int128 get_val() const { return val; }
+    Node *get_child0() const { return child0; }
+    Node *get_child1() const { return child1; }
+
 private:
     Node *parent;
     std::string key;
@@ -22,10 +29,32 @@ class TreeModel : Project
 public:
     explicit TreeModel();
 
+    void populate_nodes(int root_index);
+
     void run() override;
 
 private:
     Node *root_node;
+
+    // Masses from https://en.wikipedia.org/wiki/List_of_Solar_System_objects_by_size
+    const std::vector<std::pair<std::string, unsigned __int128>> PLANET_INFO = {
+        {"Sun", 1988500000e21},
+        {"Mercury", 330.1e21},
+        {"Venus", 4867.5e21},
+        {"Earth", 5972.4e21},
+        {"Mars", 641.7e21},
+        {"Saturn", 568340e21},
+        {"Jupiter", 1898200e21},
+        {"Neptune", 102413e21},
+        {"Uranus", 86813e21},
+        {"Pluto", 13.03e21},
+        {"Ceres", 0.938e21},
+        {"Io", 89.32e21},
+        {"Europa", 48e21},
+        {"Moon", 73.42e21},
+        {"Phobos", 10.659e15},
+        {"162173 Ryugu", 450e9}
+    };
 };
 
 
