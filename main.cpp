c0d8e77a2640b3674f509afea3be2eb017766b4b
Benjamin O <jeep70cp@gmail.com>
2020-11-29 15:13:04 -0500

    implement the long-awaited main.cpp menu system

--- main.cpp
+++ main.cpp
@@ -1,15 +1,54 @@
 #include <iostream>
 
+#include "project.h"
 #include "teamoptimizer.h"
 #include "datamodel.h"
 #include "treemodel.h"
 
+void display_main_menu()
+{
+    std::string message = "Select a project:\n \
+        [1] Team Optimizer (game dev team)\n \
+        [2] Data Model (usb devices)\n \
+        [3] Tree Model (planets)\n \
+        [4] Search Trees (interstellar scenario/final)\n \
+        [e] Exit program\n";
+
+    std::vector<char> accepted_values = {'1', '2', '3', '4', 'e'};
+
+    bool exit = false;
+    while (!exit)
+    {
+        char input_char = Project::wait_for_valid_char(message, accepted_values);
+        switch (input_char) {
+            case '1': {
+                TeamOptimizer team_optimizer;
+                team_optimizer.run();
+                break;
+            }
+            case '2': {
+                DataModel data_model;
+                data_model.run();
+                break;
+            }
+            case '3': {
+                TreeModel tree_model;
+                tree_model.run();
+                break;
+            }
+            case '4': {
+
+                break;
+            }
+            case 'e': {
+                exit = true;
+                break;
+            }
+        }
+    }
+}
+
 int main() 
 {
-//   TeamOptimizer team_optimizer;
-//   team_optimizer.run();
-//    DataModel data_model;
-//    data_model.run();
-    TreeModel tree_model;
-    tree_model.run();
+    display_main_menu();
 }
--- project.h
+++ project.h
@@ -1,10 +1,86 @@
 #ifndef PROJECT_H
 #define PROJECT_H
 
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
 class Project
 {
   public:
     virtual void run() = 0;
+
+    static char wait_for_valid_char(std::string message, std::vector<char> accepted_values)
+    {
+        while (true)
+        {
+            std::cout << message;
+
+            std::string input;
+            std::cin >> input;
+            const char input_char = tolower(input.at(0));
+
+            if(std::find(accepted_values.cbegin(), accepted_values.cend(), input_char) != accepted_values.cend()) {
+                return input_char;
+            }
+        }
+    }
+
+    static int wait_for_number(std::string message)
+    {
+        while (true)
+        {
+            std::cout << message;
+
+            std::string input;
+            std::cin >> input;
+
+            // set non_digit_present to true by checking all chars in the input string
+            bool non_digit_present = false;
+            for (const char &c : std::as_const(input))
+            {
+                if (!std::isdigit(c))
+                {
+                    non_digit_present = true;
+                    break;
+                }
+            }
+            // If there is a non digit present or the value is ridiculous, don't accept it
+            if (!non_digit_present)
+            {
+                int value = std::stoi(input);
+                if (value >= 0 && value < 99)
+                    return value;
+            }
+        }
+        return -1;
+    }
+
+    static bool yes_or_no_to_bool(char input_char)
+    {
+        if (input_char == 'y')
+        {
+            return true;
+        }
+        else if (input_char == 'n')
+        {
+            return false;
+        }
+        // Maybe this should be a std::optional
+        return false;
+    }
+
+    static std::string bool_to_yes_or_no(bool input_bool)
+    {
+        if (input_bool)
+        {
+            return "Yes";
+        }
+        else
+        {
+            return "No";
+        }
+    }
 };
 
 #endif // PROJECT_H
